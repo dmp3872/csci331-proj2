7f8c49f07061b6fcf7bf904d9ee70e1ee9a2d80d
Kellen Bell <93352579+kdb9520@users.noreply.github.com>
2023-10-22 11:53:53 -0400

    derek comments

--- painter_Derek_Pruski.py
+++ painter_Derek_Pruski.py
@@ -20,15 +20,24 @@ def recombine(
 		and then slicing each image into two pieces along
 		a randomly-chosen vertical or horizontal line.
 	"""
+	# random number 1 or 0
 	rand = random.randint(0,1)
+
+	# dimensions either length or width
 	dim = im1.shape[rand]
 
+	# find a random line to split on
 	randSplit = random.randint(0, dim)
 
+	# split horiz if 0
 	if rand == 0:
 		im1[randSplit:dim, :, :] = im2[randSplit:dim, :, :]
+
+	# split vert
 	else:
 		im1[:, randSplit:dim, :] = im2[:, randSplit:dim, :]
+
+	# return the combined image
 	return im1
 
 
@@ -45,32 +54,27 @@ def mutate(im: np.ndarray) -> np.ndarray:
 		replace with a randomly chosen new color.
 	"""
 	im = im.astype(int)
-	newcolor = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
-	# print(newcolor)
 
+	# make a new color randomly
+	newcolor = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
+
+	# find all the unique colors in the image
 	flatImage = im.reshape(im.shape[0] * im.shape[1], 3)
 	colors = [list(x) for x in flatImage]
 	colors = np.unique(colors, axis=0)
 	
+	# pick a color to replace
 	i = random.randint(0, colors.shape[0]-1)
-
-	# print(colors[i])
-
-	# x, y, z = np.where(im == colors[i])
 
 	newImage = np.copy(im)
 
 	M, N, C = im.shape
 
+	# check for the color and replace it
 	for k in range(M):
 		for j in range(N):
 			if np.array_equal(newImage[k][j], colors[i]):
 				newImage[k][j] = newcolor
-
-	# newImage[x[0]:x[-1]+1, y[0]:y[-1]+1] = newcolor
-
-	# plt.imshow(newImage)
-	# plt.waitforbuttonpress(0)
 
 	return newImage
 
@@ -88,16 +92,25 @@ def evaluate(im: np.ndarray):
 		freedom to implement this however you like.
 	"""
 	# select for more more colors
+	# get unique colors
 	flatImage = im.reshape(im.shape[0] * im.shape[1], 3)
 	colors = [list(x) for x in flatImage]
 	colors = np.unique(colors, axis=0)
+
+	# select for colors with more blue and less red
 	blue_score = 0
 	for c in colors:
+		# give points for high blue values
 		if c[2] > 200:
 			blue_score += 1
+		# take points for high red values
 		if c[0] > 150:
 			blue_score -= 1
-	return blue_score
+
+	# return blue points - red points + num of colors (color points)
+	return blue_score + colors.shape[0]
+
+
 def main():
 	parser = argparse.ArgumentParser(
     	prog='painter',
@@ -117,53 +130,41 @@ def main():
 	blue = np.zeros((400,800,3))
 	blue[:,:,2] = 255
 
+	# init the pool with red and blue images
 	initPool = [0] * args.pools
 	for i in range(args.pools):
 		if i % 2 == 0:
 			initPool[i] = [np.copy(red), 0]
 		else:
 			initPool[i] = [np.copy(blue), 0]
+
+	# loop through each generation
 	for i in range(args.generations):
+
+		# eval images and sort by points
 		for l in range(args.pools):
 			initPool[l][1] = evaluate(initPool[l][0])
 		initPool = sorted(initPool, key=lambda x: x[:][1], reverse=True)
+
+		# recombine the top pairs
 		for k in range(args.recombine):
 			if k < args.pools - 1:
 				initPool[k][0] = recombine(initPool[k][0], initPool[k+1][0])
+
+		# loop through each image with a chance of mutation
 		for j in range(args.pools):
 			mutateChance = random.uniform(0,1)
 			if mutateChance <= args.mutation:
 				initPool[j][0] = mutate(initPool[j][0])
 
+	# sort one last time
 	for l in range(args.pools):
 		initPool[l][1] = evaluate(initPool[l][0])
 	initPool = sorted(initPool, key=lambda x: x[:][1], reverse=True)
 
+	# output top 3 images
 	for i in range(0, 3):
 		plt.imsave("art{}.tiff".format(i+1), initPool[i][0]/255)
-		# plt.imshow(initPool[i][0])
-		# plt.waitforbuttonpress(0)
-
-	# # red = np.zeros((400,800,3))
-	# # red[:,:,0] = 255
-	# # plt.imsave("red.tiff", red/255)
-
-	# blue = np.zeros((400,800,3))
-	# blue[:,:,2] = 255
-	# # uncomment the lines below to view the image
-
-	# im1 = recombine(red, blue)
-	# im2 = recombine(blue, red)
-
-	# for i in range(0, 5):
-	# 	im1 = mutate(recombine(im1, im2))
-	# 	im2 = mutate(recombine(im2, im1))
-
-	# im1 = mutate(recombine(im1, im2))
-	# print(evaluate(im1))
-	# plt.imshow(im1)
-	# plt.show() 
-
 	
 if __name__ == '__main__':
 	main()
